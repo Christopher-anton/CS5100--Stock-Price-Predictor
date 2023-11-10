e2325ec80039bc0ff70b3b0414678c543dd57359
Pranav K Nair <pranavn12.11@gmail.com>
2023-11-09 20:57:04 -0500

    Small changes and adding datasets

--- code/dataset_setup.py
+++ code/dataset_setup.py
@@ -8,28 +8,25 @@ class train_data:
         self.csv = csv
         self.past = past
         self.future = future
+    
     def csv_read(self):
-     df = pd.read_csv(self.csv)
-     train_dates = pd.to_datetime(df['Date'])
-     cols = list(df)[1:6]
-     for_training = df[cols].astype(float)
+       df = pd.read_csv(self.csv)
+       train_dates = pd.to_datetime(df['Date'])
+       
+       cols = list(df)[1:6]
+       for_training = df[cols].astype(float)
 
-     scaler = StandardScaler()
-     scaler  = scaler.fit(for_training)
-     scaled_for_training = scaler.transform(for_training)
-     trainX = []
-     trainY = []
+       scaler = StandardScaler()
+       scaler  = scaler.fit(for_training)
+       scaled_for_training = scaler.transform(for_training)
 
-    #  past_days = 14
-    # future = 1
-
-     for i in range(self.past, len(scaled_for_training) -self.future +1):
-         trainX.append(scaled_for_training[i - self.past:i, 0:for_training.shape[1]])
-         trainY.append(scaled_for_training[i + self.future - 1:i +self.future, 0])
-     trainX, trainY = np.array(trainX), np.array(trainY)
-
-    #  print('trainX shape =={}.'.format(trainX.shape))
-    #  print('trainY shape == {}.'.format(trainY.shape))
-
-     return trainX, trainY
+       trainX = []
+       trainY = []
+       
+       for i in range(self.past, len(scaled_for_training) - self.future + 1):
+        trainX.append(scaled_for_training[i - self.past:i, 0:for_training.shape[1]])
+        trainY.append(scaled_for_training[i + self.future - 1:i + self.future, 0])
+       
+       trainX, trainY = np.array(trainX), np.array(trainY)
+       return trainX, trainY
     
--- code/main.py
+++ code/main.py
@@ -2,19 +2,24 @@ from dataset_setup import train_data
 from LSTM import LSTM
 import torch    
 import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
 input_dim = 5
 hidden_dim = 32
 num_layers = 2
 output_dim = 2
 
-past_days = 13
+past_days = 14
 future = 1
 
 # dateset class
-obj = train_data('A:\Academics_NEU\sem_6\TSLA.csv',past_days,future)
+obj = train_data('dataset/TSLA.csv',past_days,future)
 trainX , trainY =obj.csv_read()
-# print(trainX)
-# ***********************************
+
+# plt.plot([i for i in range(len(trainY))], trainY)
+# plt.show()
+
 # LSTM model
 model = LSTM( input_dim=input_dim, hidden_dim=hidden_dim, output_dim = output_dim, num_layers=num_layers)
 
@@ -23,14 +28,14 @@ loss_fn = torch.nn.MSELoss()
 optimiser = torch.optim.Adam(model.parameters(), lr = 0.01)
 print(model)
 print(len(list(model.parameters())))
-for i in range(len(list(model.parameters()))):
+for i in tqdm(range(len(list(model.parameters())))):
     print(list(model.parameters())[i].size())
 
 num_epochs = 100
 hist = np.zeros(num_epochs)
 seq_dim = past_days
 
-for t in range(num_epochs):
+for t in tqdm(range(num_epochs)):
     y_train_pred = model(trainX)
     loss = loss_fn(y_train_pred, trainY)
     if t%10==0 and t!=0:
